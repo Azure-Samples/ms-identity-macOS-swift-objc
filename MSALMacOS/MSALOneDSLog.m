40fecba757ccfca270218dffff7190b1ea836bf9
Swasti Gupta <swastigupta@V-AMRPAT.northamerica.corp.microsoft.com>
2022-12-07 14:51:33 -0800

    Adding OneDS SDK to SampleApp for testing

---
+++ MSALMacOS/MSALOneDSLog.m
@@ -0,0 +1,18 @@
+//
+//  MSALOneDSLog.m
+//  MSALMacOS
+//
+//  Created by Swasti Gupta on 12/7/22.
+//  Copyright © 2022 Microsoft. All rights reserved.
+//
+
+#import "MSALOneDSLog.h"
+
+@implementation MSALOneDSLog
+
+-(void) Log
+{
+    
+}
+
+@end
---
+++ MSALMacOS/MSALOneDSLogCpp.cpp
@@ -0,0 +1,16 @@
+//
+//  MSALOneDSLogCpp.cpp
+//  MSALMacOS
+//
+//  Created by Swasti Gupta on 12/7/22.
+//  Copyright © 2022 Microsoft. All rights reserved.
+//
+
+#include "MSALOneDSLogCpp.hpp"
+
+
+
+void MSALOneDSLogCpp::Log()
+{
+    
+}
